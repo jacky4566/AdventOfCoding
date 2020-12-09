26275f7bbdbd667495bb36db0c27e39412582df5
Jackson Wiebe <jacksonwiebe@me.com>
2020-12-08 21:34:39 -0700

    Hacky Solution for Part 2

--- Day 8/Day8.java
+++ Day 8/Day8.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import static java.util.stream.Collectors.toList;
 
 public class Day8 {
     public static void main(String[] args) throws IOException {
@@ -28,53 +27,37 @@ public class Day8 {
             e.printStackTrace();
         }
 
-        long accumulator = 0;
-        int codePosition = 0;
-        List<Instruction> part1Set = List.copyOf(instructionSet);
-        while (part1Set.get(codePosition).executionCounter < 1) {// run till we are done
-            part1Set.get(codePosition).executionCounter++;
-            switch (part1Set.get(codePosition).operation) {
-                case 'n': // No Op
-                    codePosition++;
-                    break;
-                case 'j': // Jump
-                    codePosition = codePosition + part1Set.get(codePosition).argument;
-                    break;
-                case 'a': // Add to accumulator
-                    accumulator = accumulator + part1Set.get(codePosition).argument;
-                    codePosition++;
-                    break;
-                default:
-                    printer = "Error";
-                    System.out.println(printer);
-                    break;
-            }
-            if (codePosition > part1Set.size()) {
-                codePosition = codePosition - part1Set.size();
-            }
-        }
-        printer = "Part 1 Accumulator " + accumulator;
-        System.out.println(printer);
-        System.out.println();
+        /*
+         * long accumulator = 0; int codePosition = 0; List<Instruction> part1Set =
+         * List.copyOf(instructionSet); while
+         * (part1Set.get(codePosition).executionCounter < 1) {// run till we are done
+         * part1Set.get(codePosition).executionCounter++; switch
+         * (part1Set.get(codePosition).operation) { case 'n': // No Op codePosition++;
+         * break; case 'j': // Jump codePosition = codePosition +
+         * part1Set.get(codePosition).argument; break; case 'a': // Add to accumulator
+         * accumulator = accumulator + part1Set.get(codePosition).argument;
+         * codePosition++; break; default: printer = "Error";
+         * System.out.println(printer); break; } if (codePosition > part1Set.size()) {
+         * codePosition = codePosition - part1Set.size(); } } printer =
+         * "Part 1 Accumulator " + accumulator; System.out.println(printer);
+         * System.out.println();
+         */
 
         // Part 2 Traverse code
-        accumulator = 0;
-        for (int i = 0; i < instructionSet.size(); i++) {
+        long accumulator = 0;
+        for (int i = 0; i < instructionSet.size(); i++) { // Go through list changing one value at a time
             printer = "Change Position " + i;
             System.out.println(printer);
-            List<Instruction> modifiedSet = instructionSet.stream().collect(toList());
-            if (modifiedSet.get(i).operation == 'j') {
-                modifiedSet.get(i).operation = 'n';
-            } else if (modifiedSet.get(i).operation == 'n') {
-                modifiedSet.get(i).operation = 'j';
-            }
+            List<Instruction> modifiedSet = List.copyOf(instructionSet); // Copy to new list. Appears broken?
+            modifiedSet.get(i).swapOP(); // Swap operations
             accumulator = execute(modifiedSet);
             if (accumulator > 0) {
                 break;
+            } else {
+                modifiedSet.get(i).swapOP();// swap back for broken copy
             }
-            for (int j = 0; j < instructionSet.size(); j++) { // dump list
-                printer = modifiedSet.get(j).operation + " " + modifiedSet.get(j).argument;
-                System.out.println(printer);
+            for (int p = 0; p < instructionSet.size(); p++) { // reset executionCounters since copy is broken
+                instructionSet.get(p).executionCounter = 0;
             }
             System.out.println();
         }
@@ -113,10 +96,32 @@ public class Day8 {
         }
         return accumulator;
     }
+
+    private static Object deepCopy(Object object) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ObjectOutputStream outputStrm = new ObjectOutputStream(outputStream);
+            outputStrm.writeObject(object);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+            ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
+            return objInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
 
 class Instruction { // defines a piece of luggage and its count
     char operation;
     int argument;
     int executionCounter = 0;
+
+    public void swapOP() {
+        if (this.operation == 'j') {
+            this.operation = 'n';
+        } else if (this.operation == 'n') {
+            this.operation = 'j';
+        }
+    }
 }
